3d73ade7d8d61922e97ee716dc70393c465c8845
spbisc97 <rotondi97simone@gmail.com>
2023-10-08 20:06:15 +0200

    reward and tests

--- tests/test_satse2.py
+++ tests/test_satse2.py
@@ -5,6 +5,35 @@ import numpy as np
 import logging
 
 logging.basicConfig(level=logging.INFO)
+k = np.array(
+    [
+        [
+            0.000212278114670,
+            -0.000083701859629,
+            0.000000000000000,
+            0.098840940992828,
+            0.018561185733915,
+            0.000000000000000,
+        ],
+        [
+            0.000133845264885,
+            0.000054717444153,
+            0.000000000000000,
+            0.018561185733917,
+            0.074573364422630,
+            0.000000000000000,
+        ],
+        [
+            0.000000000000000,
+            0.000000000000000,
+            0.000100000000000,
+            -0.000000000000000,
+            0.000000000000000,
+            0.015818032751518,
+        ],
+    ],
+    dtype=np.float32,
+)
 
 
 def test_imports():
@@ -60,8 +89,61 @@ def test_rgb_graph():
     env.close()
 
 
+def test_reward():
+    from safegym.envs import Satellite_SE2
+
+    env = Satellite_SE2(
+        underactuated=False,
+        render_mode="human",
+        max_action=np.float32(1),
+        step=np.float32(0.1),
+    )
+    env.reset()
+    rewards = []
+    done = False
+    while not done:
+        obs, reward, trunc, term, info = env.step(
+            -k @ env.chaser.get_state() * (0.4)
+        )
+        rewards.append(reward)
+        done = term or trunc
+    env.close()
+    print("Total reward: ", sum(rewards))
+    print("total steps: ", len(rewards))
+
+
+def test_trajectory():
+    from safegym.envs import Satellite_SE2
+    from matplotlib import pyplot as plt
+    from PIL import Image
+
+    env = Satellite_SE2()
+    env.reset()
+    empty = np.zeros((2,), dtype=np.uint8)
+    rewards = []
+    for i in range(100_000):
+        obs, rew, trunc, term, info = env.step(empty)
+        rewards.append(rew)
+    env.close()
+    print("Total reward: ", sum(rewards))
+
+
+def profile():
+    import cProfile as profile
+    import os
+
+    prof = profile.Profile()
+    prof.enable()
+    test_trajectory()
+    prof.disable()
+    prof.dump_stats("test_env.prof")
+    import os
+
+    os.system("snakeviz test_env.prof")
+
+
 if __name__ == "__main__":
-    test_rgb_graph()
     import sys
 
     sys.path.append("..")
+    profile()

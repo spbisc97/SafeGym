d5fa1aaed4e050c0904db281eeaa856e2b1a0d20
spbisc97 <rotondi97simone@gmail.com>
2023-10-20 15:10:42 +0200

    setuo

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="safegym",
-    version="0.11",
+    version="0.12",
     packages=find_packages(),
     package_data={"": ["typed.py"]},
     install_requires=[
